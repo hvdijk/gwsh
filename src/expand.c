312cbdf0899c7de7f3690094f632776b8315171c
Harald van Dijk <harald@gigawatt.nl>
2018-03-24 14:13:56 +0100

    Treat $@ and $* as unset when $# is zero. Include exception for set -u.

--- src/expand.c
+++ src/expand.c
@@ -761,7 +761,7 @@ vsplus:
 		goto again;
 	}
 
-	if (varlen < 0 && uflag)
+	if (varlen < 0 && uflag && (*var != '@' && *var != '*'))
 		varunset(p, var, 0, 0);
 
 	if (subtype == VSLENGTH) {
@@ -932,7 +932,7 @@ numvar:
 		else
 			sep = ifsset() ? ifsval()[0] : ' ';
 		sepc = sep;
-		if (!(ap = shellparam.p))
+		if (!*(ap = shellparam.p))
 			return -1;
 		while ((p = *ap++)) {
 			len += strtodest(p, quotes);
