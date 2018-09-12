3b2307005d9ca18f57151ba239c3a0c3c107d681
Harald van Dijk <harald@gigawatt.nl>
2018-09-12 21:07:21 +0200

    Tolerate division producing an out-of-range value.
    Treat it the same way other operators are implicitly treated: reduce the
    mathematically correct result modulo the range of intmax_t.
    
    Test case:
    
      echo $(((-0x7FFFFFFFFFFFFFFF-1)/-1))
      echo $(((-0x7FFFFFFFFFFFFFFF-1)%-1))

--- src/arith_yacc.c
+++ src/arith_yacc.c
@@ -100,6 +100,8 @@ static intmax_t do_binop(int op, intmax_t a, intmax_t b)
 	case ARITH_DIV:
 		if (!b)
 			yyerror("division by zero");
+		if (b == -1)
+			return op == ARITH_REM ? 0 : -a;
 		return op == ARITH_REM ? a % b : a / b;
 	case ARITH_MUL:
 		return a * b;
