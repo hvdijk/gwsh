dd6710a533e8686f96cf8cc5df0a1ed7b7008dec
Harald van Dijk <harald@gigawatt.nl>
2018-12-13 15:28:17 +0000

    Allow escape sequences in PSn prompt variables if libedit supports it.

--- src/histedit.c
+++ src/histedit.c
@@ -3,6 +3,8 @@
  *	The Regents of the University of California.  All rights reserved.
  * Copyright (c) 1997-2005
  *	Herbert Xu <herbert@gondor.apana.org.au>.  All rights reserved.
+ * Copyright (c) 2018
+ *	Harald van Dijk <harald@gigawatt.nl>.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Kenneth Almquist.
@@ -115,7 +117,11 @@ histedit(void)
 			if (el != NULL) {
 				if (hist)
 					el_set(el, EL_HIST, history, hist);
-				el_set(el, EL_PROMPT, getprompt);
+#ifdef EL_PROMPT_ESC
+				el_set(el, EL_PROMPT_ESC, getprompt, 1);
+#else
+				el_set(el, EL_PROMPT, getprompt, 1);
+#endif
 			} else {
 bad:
 				out2str("sh: can't initialize editing\n");
